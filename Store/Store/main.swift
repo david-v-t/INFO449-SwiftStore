32f623d48348666b98ce525839df1fb4b43a8c7a
David <davidvt@uw.edu>
2026-01-27 23:03:52 -0800

    added passing implementations and additional unit tests

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -7,13 +7,86 @@
 
 import Foundation
 
-protocol SKU {}
+protocol SKU {
+    var name: String { get }
+    func price() -> Int
+}
 
-class Item {}
+class Item: SKU {
+    let name: String
+    let priceEach: Int
+    
+    init(name: String, priceEach: Int) {
+        self.name = name
+        self.priceEach = priceEach
+    }
+    
+    func price() -> Int {
+        return priceEach
+    }
+    
+}
 
-class Receipt {}
+class Receipt {
+    var scannedItems: [SKU] = []
+    
+    func add(_ sku: SKU) {
+        scannedItems.append(sku)
+    }
+    
+    func items() -> [SKU] {
+        return scannedItems
+    }
+    
+    func convert(_ cents: Int) -> String {
+        let dollars = Double(cents) / 100.0
+        return "$" + String(format: "%.2f", dollars)
+    }
+    
+    func total() -> Int {
+        var sum = 0
+        for item in scannedItems {
+            sum += item.price()
+        }
+        return sum
+    }
+    
+    func output() -> String {
+        var result = "Receipt:\n"
+        
+        for item in scannedItems {
+            result += "\(item.name): \(convert(item.price()))\n"
+        }
+        result += "------------------\n"
+        result += "TOTAL: \(convert(total()))"
+        
+        return result
+    }
+    
+}
 
-class Register {}
+class Register {
+    var receipt: Receipt
+    
+    init() {
+        self.receipt = Receipt()
+    }
+    
+    func scan(_ sku: SKU) {
+        receipt.add(sku)
+    }
+    
+    func subtotal() -> Int {
+        return receipt.total()
+    }
+    
+    func total() -> Receipt {
+        let finalReceipt = receipt
+        receipt = Receipt()
+        
+        return finalReceipt
+    }
+}
 
 class Store {
     let version = "0.1"
--- Store/StoreTests/StoreTests.swift
+++ Store/StoreTests/StoreTests.swift
@@ -66,4 +66,53 @@ TOTAL: $7.97
 """
         XCTAssertEqual(expectedReceipt, receipt.output())
     }
+    
+    func testSingleItemSubTotal() {
+        register.scan(Item(name: "Beans", priceEach: 399))
+        XCTAssertEqual(register.subtotal(), 399)
+    }
+    
+    func testReceiptClearsAfterTotal() {
+        register.scan(Item(name: "Beans", priceEach: 399))
+        register.scan(Item(name: "Bananas", priceEach: 199))
+        
+        let receipt = register.total()
+        XCTAssertEqual(receipt.items().count, 2)
+        XCTAssertEqual(receipt.total(), 399 + 199)
+
+        XCTAssertEqual(register.subtotal(), 0)
+        
+        let emptyReceipt = register.total()
+        XCTAssertEqual(emptyReceipt.items().count, 0)
+        XCTAssertEqual(emptyReceipt.total(), 0)
+    }
+    
+    func testNoItems() {
+        XCTAssertEqual(register.subtotal(), 0)
+        
+        let receipt = register.total()
+        XCTAssertEqual(receipt.items().count, 0)
+        
+        let expectedReceipt = """
+Receipt:
+------------------
+TOTAL: $0.00
+"""
+        XCTAssertEqual(expectedReceipt, receipt.output())
+    }
+    
+    func testZeroPriceItem() {
+        
+        register.scan(Item(name: "Stickers", priceEach: 0))
+        XCTAssertEqual(register.subtotal(), 0)
+        
+        let receipt = register.total()
+        let expectedReceipt = """
+Receipt:
+Stickers: $0.00
+------------------
+TOTAL: $0.00
+"""
+        XCTAssertEqual(expectedReceipt, receipt.output())
+    }
 }
